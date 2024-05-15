5e49887f48708cef5ebb9a790f4abf220e95a287
sametion <sametacikk@gmail.com>
2024-05-15 16:45:24 +0300

    linq tutorial has added

---
+++ LinqTutorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LinqTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category>();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+   class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int CategoryId { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+    class Category
+    {
+        public int CategoryId { get; set;}
+        public string CategoryName { get; set; }
+    }
+}
